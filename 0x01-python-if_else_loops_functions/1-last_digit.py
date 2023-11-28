f0bb7d03875979c4d01b4860f453527980cc695c
CHIGODI <chiegody254@gmail.com>
2023-11-28 12:41:10 +0300

    Error fix: syntax errror

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,6 +13,6 @@ elif number > 0:
 if number > 5:
    print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif number == 0:
-   print(f"Last digit of {number} is {lastDigit} and is zero")
+   print(f"Last digit of {number} is {lastDigit} and is 0")
 elif number < 6 and number != 0:
-   print(f"Last digit of {number} is {lastDigit} and is less that 6 and not 0")
+   print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
