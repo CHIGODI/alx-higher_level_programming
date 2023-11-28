24cad71364878a27e6613bc7522749c1a3d62528
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-11-28 22:33:48 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
         ch = ord(ch)
         if (ch > 96 and ch < 123):
             ch = ch - 32
-            ch = chr(ch)
-            print("{}".format(ch), end='')
-print("")
+        ch = chr(ch)
+        print("{}".format(ch), end='')
+    print("")
