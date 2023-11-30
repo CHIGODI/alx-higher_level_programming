4429c7f323bde2aa3e205418992756374e5552bd
CHIGODI <chiegody254@gmail.com>
2023-11-30 12:26:15 +0300

    Fix: Pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import add_0 as add
 
+
 def add_numbers():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add.add(a,b)))
+    print("{} + {} = {}".format(a, b, add.add(a, b)))
+
 
 if __name__ == "__main__":
     add_numbers()
