1759be04b1cceb71fae97ce07de105675a6fc4cb
CHIGODI <chiegody254@gmail.com>
2023-11-30 21:11:19 +0300

    Fix: correct output

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,29 +3,29 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 argument = sys.argv
-operator = argument[2]
-sum_args = add(int(argument[1]), int(argument[3]))
-sub_args = sub(int(argument[1]), int(argument[3]))
-mul_args = mul(int(argument[1]), int(argument[3]))
-div_args = div(int(argument[1]), int(argument[3]))
-
+operators = {'+', '-', '*', '/'}
 
 def calculator():
     if len(argument) != 4:
         print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
-        if operator == '+':
-            print(f"{argument[1]} + {argument[3]} = {sum_args}")
-        elif operator == '-':
-            print(f"{argument[1]} - {argument[3]} = {sub_args}")
-        elif operator == '*':
-            print(f"{argument[1]} * {argument[3]} = {mul_args}")
-        elif operator == '/':
-            print(f"{argument[1]} / {argument[3]} = {div_args}")
-        else:
+        operator = argument[2]
+        if operator not in operators:
             print(f"Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
+
+        a = int(argument[1])
+        b = int(argument[3])
+
+        if operator == "+":
+            print(f"{a} + {b} = {add(a, b)}")
+        elif operator == "-":
+            print(f"{a} - {b} = {sub(a, b)}")
+        elif operator == "*":
+            print(f"{a} * {b} = {mul(a, b)}")
+        elif operator == "/":
+            print(f"{a} / {b} = {div(a, b)}")
 
 
 if __name__ == "__main__":
