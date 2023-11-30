882d656245b72b1f75ac830379e225612f89ff32
CHIGODI <chiegody254@gmail.com>
2023-11-30 12:21:02 +0300

    Implement task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import add_0 as add
+
+def add_numbers():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add.add(a,b)))
+
+if __name__ == "__main__":
+    add_numbers()
