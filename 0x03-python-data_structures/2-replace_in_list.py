865fa9577f30f5b9c8feb7650009a626891485af
CHIGODI <chiegody254@gmail.com>
2023-12-02 15:00:03 +0300

    Fix: Add if condition

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 
 
 def replace_in_list(my_list, idx, element):
-    copy_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return my_list
-    copy_list[idx] = element
-    return copy_list
+    else:
+        my_list[idx] = element
+        return my_list
