7d2647aa70717e63ce0089ccd7348836366bc074
CHIGODI <chiegody254@gmail.com>
2023-12-03 11:26:38 +0300

    Fix: pycodestyle & correct output

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,11 +2,11 @@
 
 
 def max_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None or len(my_list) == 0:
         return None
 
     large = my_list[0]
     for element in my_list:
         if element > large:
             large = element
-            return large
+    return large
