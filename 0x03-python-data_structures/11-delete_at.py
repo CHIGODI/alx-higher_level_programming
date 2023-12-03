33254bde04871eef431636af49052512739a3f7c
CHIGODI <chiegody254@gmail.com>
2023-12-03 12:22:17 +0300

    optimise

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,11 +5,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    rem = my_list[idx]
-    new = my_list[:]
-    my_list.clear()
-    for item in new:
-        if item is not rem:
-            my_list.append(item)
-
+    del my_list[idx]
     return my_list
