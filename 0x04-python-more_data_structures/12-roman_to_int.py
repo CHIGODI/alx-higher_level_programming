4b730ba3f7e35151f45849b343554106d53bb73d
CHIGODI <chiegody254@gmail.com>
2023-12-06 16:33:36 +0300

    Fix: corredt output edge case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if not roman_string or len(roman_string) == 0:
+    if type(roman_string)is not str or roman_string is None:
         return None
 
     my_dict = {
