e0962b802c8dcdabbb04e97b59e449baea418c59
CHIGODI <chiegody254@gmail.com>
2023-12-06 15:59:22 +0300

    Fix: corect ouput edge case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,11 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
     dict_copy = list(a_dictionary.items())
     best_score = dict_copy[0][1]
     keys = ""
-
     for key, value in a_dictionary.items():
         if value > best_score:
             best_score = value
