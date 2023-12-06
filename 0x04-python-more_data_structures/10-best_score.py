4c491027925e480bfd7da2c454276d099e53049b
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-12-06 23:28:05 +0300

    Fix: correct ouput

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-
-
 def best_score(a_dictionary):
     if not a_dictionary or len(a_dictionary) == 0:
         return None
-    keys = ""
-    dict_copy = list(a_dictionary.values())
-    best_score = dict_copy[0]
-    key for key, value in a_dictionary.items():
-        if value > best_score:
-            best_score = value
-            keys = key
-    return keys
+    return max(a_dictionary, key=a_dictionary.get)
