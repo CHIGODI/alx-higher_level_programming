50e3f9d630936915012fc33a100f85513c093b47
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-12-06 23:15:09 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,10 +4,10 @@
 def best_score(a_dictionary):
     if not a_dictionary or len(a_dictionary) == 0:
         return None
-    dict_copy = list(a_dictionary.items())
-    best_score = dict_copy[0][1]
     keys = ""
-    for key, value in a_dictionary.items():
+    dict_copy = list(a_dictionary.values())
+    best_score = dict_copy[0]
+    key for key, value in a_dictionary.items():
         if value > best_score:
             best_score = value
             keys = key
