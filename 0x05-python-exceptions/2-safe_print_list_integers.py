efbaffdd489338dac806c7d31505dd9329fff4e7
CHIGODI <chiegody254@gmail.com>
2023-12-18 23:40:39 +0300

    Fix: error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         print("{:d}".format(mylist[count]))
         count += 1
     except Exception:
-        pass
+        continue
 
     print()
     return (count)
