59c704b5310d450a0a71f9802adf3bc90bbdebc7
CHIGODI <chiegody254@gmail.com>
2023-12-18 23:35:09 +0300

    Implement task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0;
+    while count < x:
+        print("{:d}".format(mylist[count]))
+        count += 1
+    except Exception:
+        pass
+
+    print()
+    return (count)
