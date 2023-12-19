a017b6894138f4717918a25a8c7a2059df01b0de
CHIGODI <chiegody254@gmail.com>
2023-12-19 12:43:44 +0300

    Implement task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""A class Square that defines a square."""
+
+
+class Square:
+    """Represents a square object."""
+
+    def __init__(self, size):
+        """
+        Initializes a Square object.
+
+        Parameters:
+        size (int): The size of the square.
+
+        Note:
+        __size is a private attribute.
+        """
+        self.__size = size
