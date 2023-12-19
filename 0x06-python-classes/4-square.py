bfad77297be62f45bec74104b4fd28fdabaabae4
CHIGODI <chiegody254@gmail.com>
2023-12-19 14:54:11 +0300

    Fix: error message

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,9 +23,9 @@ class Square:
     def size(self, value):
         """Setter method to set the value of size."""
         if type(value) is not int:
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
