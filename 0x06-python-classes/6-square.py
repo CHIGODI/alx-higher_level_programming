7492d61b985ced35997c0f3daadcd84856340ee9
CHIGODI <chiegody254@gmail.com>
2023-12-29 15:41:02 +0300

    Fix: my_print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,8 +55,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__size):
-                if i == 1:
-                    print("\n" * self.__position[i], end="")
+            for i in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
