304966bd2892c2bf1e1d5511b9c5d236b62b50f3
CHIGODI <chiegody254@gmail.com>
2024-01-04 15:43:53 +0300

    Fix: pycodestyle

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -68,7 +68,8 @@ class Rectangle:
         """ makes the rectangle printable """
         if self.__width == 0 or self.__height == 0:
             return ''
-        return '\n'.join(str(self.print_symbol) * self.__width for _ in range(self.__height))
+        return '\n'.join(str(self.print_symbol) * self.__width for _ in \
+                             range(self.__height))
 
     def __repr__(self):
         """ returns the true code that produces object """
