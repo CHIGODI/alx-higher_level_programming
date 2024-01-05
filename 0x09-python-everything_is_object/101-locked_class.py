db88cdb4c5836d54cd37a96385d0d88c331dc064
CHIGODI <chiegody254@gmail.com>
2024-01-05 14:18:36 +0300

    Fix: pycodetyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This class is locked for firstname only! """
 
+
 class LockedClass:
     """ only first name instance attribute allowed """
     __slots__ = ('first_name', )
