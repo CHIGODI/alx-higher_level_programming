3b3ecb0ce5423705099ec9ad4bc80e813cc70ed4
CHIGODI <chiegody254@gmail.com>
2024-01-05 15:53:02 +0300

    Fix: nothing

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#!/usr/python3
-def magic_string(my_string=[]):
-    my_string += ['BestSchool']
-    return ', '.join(my_string)
+#!/usr/bin/python3
+def magic_string(my_str=[]):
+    my_str += ["BestSchool"]
+    return ", ".join(my_str)
