5ce6c254638ec5440d01056802300b39171f4176
CHIGODI <chiegody254@gmail.com>
2024-01-08 16:23:26 +0300

    name is always string

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,9 +16,10 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """
-        validates value
+        validates value if its ONLY int
         """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+        if isinstance(name, str):
+            if type(value) is not int:
+                raise TypeError(f"{name} must be an integer")
+            if value <= 0:
+                raise ValueError(f"{name} must be greater than 0")
