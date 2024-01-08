9fde7d64fc6bff2051c98b3c1430ee6a55f44fb6
CHIGODI <chiegody254@gmail.com>
2024-01-08 11:09:24 +0300

    Test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,9 @@ class MyList(list):
     Inherits from list and prints the list but sorted
     """
     def print_sorted(self):
-        sorted_list = self[:]
-        sorted_list.sort()
-        print(sorted_list)
+        if all(isinstance(value, int) for value in self):
+            sorted_list = self[:]
+            sorted_list.sort()
+            print(sorted_list)
+        else:
+            return None
