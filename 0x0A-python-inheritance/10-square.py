fcfc40b15117e8c5188b2bbc959d7952fde4f369
CHIGODI <chiegody254@gmail.com>
2024-01-08 22:11:45 +0300

    Fix: Use own calls

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -17,4 +17,6 @@ class Square(Rectangle):
         """
         Initializes a Square instance.
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
