319b4d6195b90c44e8332f8993e3b94ffa3409c5
CHIGODI <chiegody254@gmail.com>
2024-01-08 14:56:08 +0300

    Fix: add tests

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,8 +18,8 @@ class BaseGeometry:
         """
         validates value
         """
-        self.name = name
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+        if isinstance(name, str):
+            if not isinstance(value, int):
+                raise TypeError(f"{name} must be an integer")
+            if value <= 0:
+                raise ValueError(f"{name} must be greater than 0")
