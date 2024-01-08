0d424ebceb0e0393f15f93c74d09c3859c9e6493
CHIGODI <chiegody254@gmail.com>
2024-01-08 21:15:28 +0300

    Import after module comment

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 """
   This module defines the Rectangle class that represents a
   rectangle object.
   It inherits functionality from the BaseGeometry class to
   handle geometry-related operations.
 """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
