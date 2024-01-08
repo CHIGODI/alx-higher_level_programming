72ff645b0c7b56405a927eec9eedc5436bca5f43
CHIGODI <chiegody254@gmail.com>
2024-01-08 22:16:51 +0300

    Square #2 pycodestyle fix

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -20,6 +20,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
     def area(self):
         """
         Calculate area of square
