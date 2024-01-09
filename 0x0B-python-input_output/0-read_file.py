790f1ecf33c1d8e2a3290b0d67aa0bffecdbe285
CHIGODI <chiegody254@gmail.com>
2024-01-09 11:31:20 +0300

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains code that opens a file and read from
+it. prints what is read to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    opens a file to read and print contents to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        file_contents = f.read()
+    print(file_contents)
