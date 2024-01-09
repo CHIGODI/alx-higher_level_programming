751eebf7a90446eb6eae4e270002ab83bb472ad4
CHIGODI <chiegody254@gmail.com>
2024-01-09 12:33:15 +0300

    Fix: no new line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
     """
     with open(filename, encoding="utf-8") as f:
         file_contents = f.read()
-    print(file_contents)
+        print(file_contents, end='')
