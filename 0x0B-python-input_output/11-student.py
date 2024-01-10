7b9434b017b66a768e371f7c307e262567678827
CHIGODI <chiegody254@gmail.com>
2024-01-10 09:26:25 +0300

    Student to disk reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student by:
+
+Public instance attributes:
+first_name
+last_name
+age
+"""
+
+import json
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        object constructor
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        class to json
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            store = self.__dict__
+            result = {key: store[key] for key in attrs if key in store}
+            return result
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance:
+        """
+        self.__dict__ = json
+        return self
