689be09ce183eb49a2e57d72a95fd63ab163fa0d
CHIGODI <chiegody254@gmail.com>
2024-01-16 10:35:19 +0300

    Add tests #7

--- 0x0C-python-almost_a_circle/tests/test_task_7.py
+++ 0x0C-python-almost_a_circle/tests/test_task_7.py
@@ -27,10 +27,20 @@ class TestRectangleDisplay(unittest.TestCase):
         sys.stdout.truncate(0)
         sys.stdout.seek(0)
 
-        r2 = Rectangle(3, 2, 1, 0)
+        r2 = Rectangle(3, 2, 1)
         r2.display()
         expected_output_r2 = ' ###\n ###\n'
         self.assertEqual(sys.stdout.getvalue(), expected_output_r2)
+
+        # clear previous caputired output
+        sys.stdout.truncate(0)
+        sys.stdout.seek(0)
+
+        r3 = Rectangle(3, 2, 0, 1)
+        r3.display()
+        expected_output_r2 = '\n###\n###\n'
+        self.assertEqual(sys.stdout.getvalue(), expected_output_r2)
+
 
 if __name__ == '__main__':
     unittest.main()
