a0864410a0e0f964088ad1b7fe792b9887a7e437
CHIGODI <chiegody254@gmail.com>
2024-01-16 08:07:16 +0300

    square tests #16

--- 0x0C-python-almost_a_circle/tests/test_task_16.py
+++ 0x0C-python-almost_a_circle/tests/test_task_16.py
@@ -4,6 +4,7 @@ import unittest
 import os
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 import json
 
 
@@ -11,6 +12,7 @@ class TestBaseSaveToFile(unittest.TestCase):
     def tearDown(self):
         if os.path.exists("Rectangle.json"):
             os.remove("Rectangle.json")
+            Base._Base__nb_objects = 0
 
     def test_save_to_file_empty(self):
         Base.save_to_file(None)
@@ -33,6 +35,17 @@ class TestBaseSaveToFile(unittest.TestCase):
         self.assertEqual(result, [{"y": 8, "x": 2, "id": 1, "width": 10,"height"
                                    : 7}, {"y": 0, "x": 0, "id": 2, "width": 2,
                                           "height": 4}])
+    def test_save_to_file_square(self):
+        s1 = Square(10, 2, 8)
+        s2 = Square(2)
+        Square.save_to_file([s1, s2])
+        filename = 'Square.json'
+        with open(filename, mode='r', encoding='utf-8') as f:
+            result = json.load(f)
+
+        self.assertTrue(os.path.exists("Square.json"))
+        self.assertEqual(result, [{"id": 1, "size": 10, "x": 2, "y": 8},
+                                  {"id": 2, "size": 2, "x": 0, "y": 0}])
 
 if __name__ == '__main__':
     unittest.main()
