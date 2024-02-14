1971abdb63fbad923a316f36d6ae32f230b92e3d
CHIGODI <chiegody254@gmail.com>
2024-02-14 13:59:35 +0300

    correct pwd

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,8 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- creats user user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED WITH mysql_native_password BY ' user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant select privilege to user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
