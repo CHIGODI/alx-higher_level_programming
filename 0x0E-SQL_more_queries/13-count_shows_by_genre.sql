a1b18525dac4a8d60fcc80531bc672099cd711c1
CHIGODI <chiegody254@gmail.com>
2024-02-20 13:24:05 +0300

    make sql key word capital

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- display count of grouped entries
 
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id)
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id)
 AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
