443e0b89730400df13e77b34642aa5bb500bbd57
CHIGODI <chiegody254@gmail.com>
2024-02-20 13:21:27 +0300

    Fix requiremnt fail line over 80 chars

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- display count of grouped entries
 
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_of_shows 
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id)
+AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
