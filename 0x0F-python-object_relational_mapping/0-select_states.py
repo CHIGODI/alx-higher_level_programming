41af1b4209642c788ff30160dcf5c1aa4420d83a
CHIGODI <chiegody254@gmail.com>
2024-03-14 11:27:31 +0300

    Add all task files Task 0: filter states done

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa sorted by id
+accepts command line args:
+                          -MySQl username
+                          -MySQL user passwd
+                          -Database name
+"""
+import MySQLdb
+import sys
+
+argv = sys.argv
+mysql_username = argv[1]
+mysql_passwd = argv[2]
+database_name = argv[3]
+
+conn = MySQLdb.connect(host='localhost', port=3306, user=mysql_username,
+                       passwd=mysql_passwd, db=database_name)
+cur = conn.cursor()
+
+cur.execute('SELECT * FROM  states ORDER BY id')
+query_columns = cur.fetchall()
+
+for row in query_columns:
+    print(row)
+
+cur.close()
+conn.close()
