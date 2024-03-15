d1b1fa2db307e337ad96a1a5c4a8e525e0569fcb
CHIGODI <chiegody254@gmail.com>
2024-03-15 18:08:24 +0300

    Add constraints for table columns on class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """
-Class defination of state
+Class defination of State inherits from Base
 """
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import (create_engine)
 
-if __name__ == '__main__':
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import Column, Integer, String
+Base = declarative_base()
 
-    Base = declarative_base()
 
-    class State(Base):
-        """
-        initialises table attributes
-        """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True)
-        name = Column(String(50))
+class State(Base):
+    """
+    initialises table attributes
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                unique=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+# if __name__ == "__main__":
+#     engine = create_engine('mysql+mysqldb://'
+#                            'root:3778@localhost:3306/'
+#                            'hbtn_0e_6_usa')
+#     Base.metadata.create_all(engine)
