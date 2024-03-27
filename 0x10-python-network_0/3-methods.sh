bca29f484b19ae02c92badf552fcca91ea7552f7
CHIGODI <chiegody254@gmail.com>
2024-03-27 14:45:47 +0300

    awk -F add space

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes in a URL and displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | awk -F ':' '/Allow/ {print $2}'
+curl -sI -X OPTIONS "$1" | awk -F ': ' '/Allow/ {print $2}'
