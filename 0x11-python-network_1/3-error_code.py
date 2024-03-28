b0a945afda4bc1f824bcfd604684e22607ea469c
CHIGODI <chiegody254@gmail.com>
2024-03-28 10:31:20 +0300

    Add task 3: prints body or error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+or display error code
+"""
+
+import sys
+import urllib.request
+
+
+def main():
+    """
+    request to the URL and displays the body of the response
+    (decoded in utf-8). or display error code
+    """
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as req:
+            response = req.read()
+            content_utf = response.decode('utf-8')
+            print(content_utf)
+    except urllib.error.URLError as e:
+        print(f'Error code: {e.code}')
+
+
+if __name__ == '__main__':
+    main()
+            
