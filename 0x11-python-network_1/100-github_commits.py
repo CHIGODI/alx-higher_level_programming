90801d7b236d3f562c40c4332d1b0c2c7abd6cce
CHIGODI <chiegody254@gmail.com>
2024-03-28 16:08:42 +0300

    list commits API

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+Uses the GitHub API, here is the documentation:
+    - https://developer.github.com/v3/repos/commits/
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits'
+    header = {"Accept": "application/vnd.github+json"}
+    response = requests.get(url, headers=header)
+    res = response.json()
+
+    for i in range(1, 11):
+        print(
+            f"{res[i].get('sha')}: "
+            f"{res[i].get('commit').get('author').get('name')}"
+            )
