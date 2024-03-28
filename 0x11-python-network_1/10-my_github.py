b7b3d74edb1a3ba91ab9ef3fd6755916ab5ae2f1
CHIGODI <chiegody254@gmail.com>
2024-03-28 16:23:04 +0300

    change request method to get

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,5 +10,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == '__main__':
     basic = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    response = requests.post('https://api.github.com/user', auth=basic)
+    response = requests.get('https://api.github.com/user', auth=basic)
     print(response.json().get('id'))
