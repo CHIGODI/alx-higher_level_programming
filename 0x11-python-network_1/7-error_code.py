6ce1ee2b5b94fa847922b1ec5859a0fa3a8510ba
CHIGODI <chiegody254@gmail.com>
2024-03-28 11:35:46 +0300

    Add task 6: make executable Add task 7: print url body resp/err code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL, sends a request to the URL and displays the
+body of the response.
+If the HTTP status code is greater than or equal to 400,
+    - prints: Error code: followed by the value of the HTTP status code
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+
+    if response.status_code >= 400:
+        print(f'Error code: {response.status_code}')
+    print(response.text)
