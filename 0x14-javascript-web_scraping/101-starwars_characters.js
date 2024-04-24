2825a4287d3d160bc7134341d4848925fa2a1c08
CHIGODI <chiegody254@gmail.com>
2024-04-24 15:52:03 +0300

    Use async and promises

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,17 +7,41 @@ const request = require('request');
 const URL_FILM = 'https://swapi-api.alx-tools.com/api/films/';
 const MOVIE_ID = process.argv[2];
 
-request(URL_FILM, { json: true }, (err, res, body) => {
-  if (err) {
-    throw err;
-  }
-  const charactersUrls = body.results[MOVIE_ID - 1].characters;
-  charactersUrls.forEach((actorUrl) => {
-    request(actorUrl, { json: true }, (err, res, body) => {
-      if (err) {
-        throw err;
-      }
-      console.log(body.name);
+async function fetchCharacters () {
+  try {
+    const filmResponse = await new Promise((resolve, reject) => {
+      request(URL_FILM, { json: true }, (err, res, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(body);
+        }
+      });
     });
-  });
-});
+
+    const charactersUrls = filmResponse.results[MOVIE_ID - 1].characters;
+    const names = [];
+
+    for (const actorUrl of charactersUrls) {
+      const actorResponse = await new Promise((resolve, reject) => {
+        request(actorUrl, { json: true }, (err, res, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(body.name);
+          }
+        });
+      });
+      names.push(actorResponse);
+    }
+
+    // Log the character names
+    names.forEach(name => {
+      console.log(name);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fetchCharacters();
