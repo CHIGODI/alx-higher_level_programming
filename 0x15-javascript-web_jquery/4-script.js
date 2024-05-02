2e3bc8d6dd6f343c8e6b45dd5a9a9ce23547fc96
CHIGODI <chiegody254@gmail.com>
2024-05-02 14:07:35 +0300

    Add task 5 Make task 4 code short

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,13 +1,6 @@
 $(function () {
-    let element = $('#toggle_header');
-    element.addClass('red')
-    element.click(function () { 
-        if (element.hasClass('red')) { 
-            element.removeClass('red');
-            element.addClass('green');
-        } else if (element.hasClass('green')) { 
-            element.removeClass('green');
-            element.addClass('red');
-        }
+    $('#toggle_header').addClass('red')
+    $('#toggle_header').click(function () {
+        $(this).toggleClass('red green');
     });
 });
